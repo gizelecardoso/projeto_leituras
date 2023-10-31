26dee4e7561a5a662af40387e06bd2f5e817d1a9
Gizele Santos <gizele.ccardoso@gmail.com>
2023-10-30 23:39:42 -0300

    config: Add linters: rubocop, erb-lint and brakeman

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,15 +1,16 @@
+# frozen_string_literal: true
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 require 'simplecov'
 SimpleCov.start 'rails'
-
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
@@ -42,7 +43,7 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.fixture_path = Rails.root.join('path/to')
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
