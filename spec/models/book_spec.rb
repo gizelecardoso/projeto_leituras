120ece40f32cab5505543ebc30ec6ca6e09316a8
Gizele Santos <gizele.ccardoso@gmail.com>
2023-10-30 23:58:36 -0300

    teste: First test: Validations on model Book

---
+++ spec/models/book_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+RSpec.describe Book do
+  describe "model validations" do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:author) }
+    it { is_expected.to validate_presence_of(:page_quantity) }
+
+    it { is_expected.to validate_numericality_of(:page_quantity).only_integer.is_greater_than(0) }
+  end
+end
